094b13ebf6243a9f9e6ce05be1a71e7f9ba07e5a
gabriel <gabriel.f.lazari@gmail.com>
2023-01-27 10:58:12 -0300

    section home

--- menu.js
+++ menu.js
@@ -1,8 +1,15 @@
 const menu = document.querySelector('[menu]');
 const botao = document.querySelector('[botao]');
 const lista = document.querySelector('[lista]');
+const linkMenu = document.querySelectorAll('[link]');
 
 menu.addEventListener('click', () => {
     botao.classList.toggle('botao-animation');
     lista.classList.toggle('lista-ativa');
+})
+
+linkMenu.forEach(Element => {
+    Element.addEventListener('click', () => {
+        lista.classList.toggle('lista-ativa');
+    })
 })
