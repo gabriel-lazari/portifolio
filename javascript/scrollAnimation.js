b1675a60669b33ef4c32defe255326c87e1de859
gabriel <gabriel.f.lazari@gmail.com>
2023-01-30 23:36:42 -0300

    responsivo

--- javascript/scrollAnimation.js
+++ javascript/scrollAnimation.js
@@ -1,7 +1,7 @@
 const target = document.querySelectorAll('[data-anime]')
 
 function animeScroll() {
-    const windowsTop = window.pageYOffset + (window.innerHeight * .85);
+    const windowsTop = window.pageYOffset + (window.innerHeight * .75);
     target.forEach(Element => {
         if (windowsTop > Element.offsetTop) {
             Element.classList.add('animation');
